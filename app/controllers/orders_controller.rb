eb72d0cf29401232efe5a102d59f43cdfbe2e496
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-10-10 17:33:17 +0200

    creating order view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,8 +3,22 @@ class OrdersController < ApplicationController
   end
 
   def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      puts "Hourrraaayyyy"
+    end
+    redirect_to edit_order_path(@order)
   end
 
   def edit
   end
+
+  def order_params
+    params.require(:order).permit(:quantity)
+  end
+
 end
